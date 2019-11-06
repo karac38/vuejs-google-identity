115acfad8529df223f7fe3995dcf03e00a9abf9d
Pasha Karasyuk <karac38@gmail.com>
2019-11-05 22:18:23 -0500

    part 6 - Calling an HTTP API from Vue.js

--- vue-app/src/views/About.vue
+++ vue-app/src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for="(item, i) in values" :key="i">
+      <p>{{item}}</p>
+    </div>
+    <button @click="callApi">Call Api</button>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+      values: ["no data yet"]
+    }
+  },
+  methods:{
+    async callApi(){
+      await axios.get("https://localhost:5000/api/values").then(response => {
+        this.values = response.data;
+      }).catch(error => {
+        this.values.push("oops "+err);
+      })
+    }
+  }
+}
+</script>
