38ece634acb7722db1f230143da493eb0139edab
Pasha Karasyuk <karac38@gmail.com>
2019-11-05 21:12:59 -0500

    part 4 - secured vue api with identity server

--- VueApi/Startup.cs
+++ VueApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace VueApi
 {
@@ -26,6 +28,13 @@ namespace VueApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();//https://stackoverflow.com/questions/36522773/how-to-make-identityserver-to-add-user-identity-to-the-access-token
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddIdentityServerAuthentication(options =>
+                    {
+                        options.Authority = "https://localhost:5443/";
+                        options.RequireHttpsMetadata = true;
+                        options.ApiName = "api1";
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +44,15 @@ namespace VueApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            // else
+            // {
+            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //     app.UseHsts(); //https://stackoverflow.com/questions/51093139/what-is-the-difference-between-app-usehsts-and-app-useexceptionhandler
+            //https://stackoverflow.com/questions/34108241/non-authoritative-reason-header-field-http
+            // }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
